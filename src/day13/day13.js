0c5421599484e776d65164f0c9a979673da23dee
Semone Kallin Thander <semoneclarke@gmail.com>
2017-12-13 20:45:09 +0100

    Day 13 removes some lines

--- src/day13/day13.js
+++ src/day13/day13.js
@@ -1,55 +1,3 @@
-function solvePart1 (input) {
-  const cleanedInput = cleanInput(input)
-
-  const numberOfLayers = Math.max(...Object.keys(cleanedInput))
-  let severies = []
-  let isCaughtByFW
-
-  for (let index = 0; index <= numberOfLayers; index++) {
-    if (!cleanedInput[index]) {
-      isCaughtByFW = false
-    } else {
-      isCaughtByFW = isCaught(index, cleanedInput[index])
-    }
-
-    if (isCaughtByFW) {
-      severies.push(index * cleanedInput[index])
-    }
-  }
-
-  return severies.reduce((sum, el) => sum + el)
-}
-
-function solvePart2 (input) {
-  const cleanedInput = cleanInput(input)
-  const numberOfLayers = Math.max(...Object.keys(cleanedInput))
-  let isCaughtByFW = false
-  let isCaughtSomewhere = true
-
-  let delay = 0
-
-  while (isCaughtSomewhere) {
-    for (let i = 0; i <= numberOfLayers; i++) {
-      if (!cleanedInput[i]) {
-        isCaughtByFW = false
-      } else {
-        isCaughtByFW = isCaught(i, cleanedInput[i], delay)
-      }
-      if (isCaughtByFW) {
-        break
-      }
-    }
-
-    if (!isCaughtByFW) {
-      isCaughtSomewhere = false
-    } else {
-      delay++
-    }
-  }
-
-  return delay
-}
-
 function isCaught (picoSecond, range, delay = 0) {
   if ((picoSecond + delay) % (range - 1) === 0 && ((picoSecond + delay) / (range - 1)) % 2 === 0) {
     return true
@@ -65,6 +13,48 @@ function cleanInput (input) {
       obj[element.split(': ')[0]] = parseInt(element.split(': ')[1])
       return obj
     }, {})
+}
+
+function solvePart1 (input) {
+  const cleanedInput = cleanInput(input)
+
+  const numberOfLayers = Math.max(...Object.keys(cleanedInput))
+  let severies = []
+
+  for (let index = 0; index <= numberOfLayers; index++) {
+    if (isCaught(index, cleanedInput[index])) {
+      severies.push(index * cleanedInput[index])
+    }
+  }
+
+  return severies.reduce((sum, el) => sum + el)
+}
+
+function solvePart2 (input) {
+  const cleanedInput = cleanInput(input)
+  const numberOfLayers = Math.max(...Object.keys(cleanedInput))
+  let isCaughtSomewhere = true
+
+  let delay = 0
+
+  while (isCaughtSomewhere) {
+    let isCaughtByFW = false
+
+    for (let i = 0; i <= numberOfLayers; i++) {
+      if (isCaught(i, cleanedInput[i], delay)) {
+        isCaughtByFW = true
+        break
+      }
+    }
+
+    if (!isCaughtByFW) {
+      isCaughtSomewhere = false
+    } else {
+      delay++
+    }
+  }
+
+  return delay
 }
 
 module.exports = {
