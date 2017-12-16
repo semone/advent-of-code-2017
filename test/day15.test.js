6e7f694a0768a0b2047d127cc812799e998b4627
Semone Kallin Thander <semoneclarke@gmail.com>
2017-12-16 12:30:26 +0100

    Day 15 fix tests

--- test/day15.test.js
+++ test/day15.test.js
@@ -4,16 +4,16 @@ const day15 = require('../src/day15/day15')
 
 const input = [65, 8921]
 
-// describe('Dueling Generators Part 1', () => {
-//   it('shall return correct sum', () => {
-//     const result = day15.solvePart1(input)
-//     assert.equal(result, 588)
-//   })
-// })
+describe('Dueling Generators Part 1', () => {
+  it('shall return correct final count', () => {
+    const result = day15.solvePart1(input)
+    assert.equal(result, 588)
+  }).timeout(10000)
+})
 
-// describe('Dueling Generators Part 2', () => {
-//   it('shall return correct number of groups', () => {
-//     const result = day15.solvePart2(input)
-//     assert.equal(result, 309)
-//   })
-// })
+describe('Dueling Generators Part 2', () => {
+  it('shall return correct final count', () => {
+    const result = day15.solvePart2(input)
+    assert.equal(result, 309)
+  }).timeout(10000)
+})
