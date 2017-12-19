92c51479d2373c625df576aa41be5e9293aa42c3
Semone Kallin Thander <semoneclarke@gmail.com>
2017-12-19 22:37:47 +0100

    Day 19 fixes

--- src/day19/day19.js
+++ src/day19/day19.js
@@ -1,24 +1,36 @@
-function direction (map, position) {
+function changeDirection (map, position) {
   if (map[position.y][position.x] === '+') {
-    if (position.y - 1 > 0 && map[position.y - 1][position.x] && map[position.y - 1][position.x] !== ' ' && position.direction !== 1) {
+    if (position.y - 1 > 0 && !isEmpthyPath(map, position.x, position.y - 1) && position.direction !== 1) {
       position.direction = 3
       return
     }
 
-    if (position.x - 1 > 0 && map[position.y][position.x - 1] && map[position.y][position.x - 1] !== ' ' && position.direction !== 2) {
+    if (position.x - 1 > 0 && !isEmpthyPath(map, position.x - 1, position.y) && position.direction !== 2) {
       position.direction = 4
       return
     }
 
-    if (position.y + 1 < map.length && map[position.y + 1][position.x] && map[position.y + 1][position.x] !== ' ' && position.direction !== 3) {
+    if (position.y + 1 < map.length && !isEmpthyPath(map, position.x, position.y + 1) && position.direction !== 3) {
       position.direction = 1
       return
     }
 
-    if (position.x + 1 < map[position.y].length && map[position.y][position.x + 1] && map[position.y][position.x + 1] !== ' ' && position.direction !== 4) {
+    if (position.x + 1 < map[position.y].length && !isEmpthyPath(map, position.x + 1, position.y) && position.direction !== 4) {
       position.direction = 2
     }
   }
+}
+
+function isOutsideMap (map, x, y) {
+  return x > map[y].length || y > map.length || y < 0 || x < 0
+}
+
+function isEmpthyPath (map, x, y) {
+  return !map[y][x] || map[y][x] === ' '
+}
+
+function isLetter (map, position) {
+  return !isEmpthyPath(map, position.x, position.y) && map[position.y][position.x].match(/[A-Z]/i)
 }
 
 function solve (input) {
@@ -34,37 +46,32 @@ function solve (input) {
   }
   let result = ['', 0]
 
-  while (true) {
+  do {
     if (position.direction === 1) {
       position.y++
-      result[1]++
     }
 
     if (position.direction === 2) {
       position.x++
-      result[1]++
     }
 
     if (position.direction === 3) {
       position.y--
-      result[1]++
     }
 
     if (position.direction === 4) {
       position.x--
-      result[1]++
     }
 
-    direction(mapSplit, position)
+    changeDirection(mapSplit, position)
 
-    if (position.x > mapSplit[position.y].length || position.y > mapSplit.length || position.y < 0 || position.x < 0 || mapSplit[position.y][position.x] === ' ') {
-      break
-    }
+    result[1]++
 
-    if (mapSplit[position.y][position.x] && mapSplit[position.y][position.x].match(/[A-Z]/i)) {
+    if (isLetter(mapSplit, position)) {
       result[0] += mapSplit[position.y][position.x]
     }
-  }
+  } while (!isOutsideMap(mapSplit, position.x, position.y) && !isEmpthyPath(mapSplit, position.x, position.y))
+
   return result
 }
 
